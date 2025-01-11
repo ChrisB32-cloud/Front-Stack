dcff77f2d3b592701ce13b2d8cc341fb350ebc01
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2025-01-11 11:11:27 -0600

    changes

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fODxSjWsYZ0VabUc8JfXtROZsMEKgK9HabFfuOJDUv8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Qwvnj+H9fUhwBDu1Exg6j1dqVHDiq8aWaCm6cTVqxA4=\"\n}"
