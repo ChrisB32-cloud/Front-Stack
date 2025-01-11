6c90300de4850fb51cd07b67051c74d1f5c30488
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2025-01-11 11:57:53 -0600

    frontstack conversion

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -18,11 +18,11 @@ const dmSans = DM_Sans({
 
 export const metadata = {
   title: {
-    template: '%s - DeceptiConf',
-    default: 'DeceptiConf - A community-driven design conference',
+    template: '%s - Front stack designs',
+    default: 'Front stack designs - A community-driven design conference',
   },
   description:
-    'At DeceptiConf you’ll learn about the latest dark patterns being developed to trick even the smartest visitors, and you’ll learn how to deploy them without ever being detected.',
+    'At Front stack designs you’ll learn about the latest dark patterns being developed to trick even the smartest visitors, and you’ll learn how to deploy them without ever being detected.',
 }
 
 export default function RootLayout({ children }) {
@@ -36,7 +36,7 @@ export default function RootLayout({ children }) {
       )}
     >
       <body className="flex min-h-full">
-        <div className="flex w-full flex-col">{children}</div>
+        <div className="flex flex-col w-full">{children}</div>
       </body>
     </html>
   )
